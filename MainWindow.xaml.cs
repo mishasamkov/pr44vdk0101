ea7e82218ac0641aa257c8f7780b8f4550623481
csamk <csamk@SUMMRSXO>
2025-01-08 22:42:02 +0500

    создано главное окно и структура проекта самков

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace TaskManager_samkov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            DataContext = new VM_Pages();
         }
     }
 }
