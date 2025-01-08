bee77e58db48324ca98eeacb7f0a3fff9d9ca322
csamk <csamk@SUMMRSXO>
2025-01-08 22:46:50 +0500

    реализовна папка с классами

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+namespace TaskManager_samkov.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=localhost;port=3306;uid=root;database=TaskManager;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
